404cbb7939f122ebcea088b1d15323d593dbb810
Serpentarius13 <serpentarium13@mail.ru>
2022-11-27 00:16:54 +0300

    changed url of backend

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const link = new HttpLink({
-  uri: "http://localhost:400",
+  uri: "https://dream-workshop.herokuapp.com/",
   credentials: "same-origin",
 });
 
