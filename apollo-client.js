1ebbf5312bac49e0f81ae9846272c225e3cc31a4
Serpentarius13 <serpentarium13@mail.ru>
2022-11-27 02:27:14 +0300

    !

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.serverUrl || "http://localhost:4000",
+  uri: 'https://dream-workshop.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
