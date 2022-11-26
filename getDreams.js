1157c8bd7fc4217ffd5fc2da7e5e83b1e300df62
Serpentarius13 <serpentarium13@mail.ru>
2022-11-27 00:30:33 +0300

    !

--- getDreams.js
+++ getDreams.js
@@ -2,20 +2,23 @@ import client from "./apollo-client";
 import { gql } from "@apollo/client";
 
 export async function getDreams() {
-  const { data } = await client.query({
-    query: gql`
-      query Query {
-        getAll {
-          name
-          time
-          email
-          dreamName
-          description
-          _id
+  try {
+    const { data } = await client.query({
+      query: gql`
+        query Query {
+          getAll {
+            name
+            time
+            email
+            dreamName
+            description
+            _id
+          }
         }
-      }
-    `,
-  });
-
-  return data.getAll;
+      `,
+    });
+    return data.getAll;
+  } catch (err) {
+    console.log(err);
+  }
 }
